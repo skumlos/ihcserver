15579b4a7bcc7ac8ee999edb85d02b58e34edfba
Martin Hejnfelt <martin@hejnfelt.com>
2013-04-18 21:15:28 +0200

    Added webinterface to release script

--- create-release.sh
+++ create-release.sh
@@ -7,4 +7,4 @@ fi
 
 echo "Creating ihcserver-$1.tar.gz";
 
-tar -czf ihcserver-$1.tar.gz *.cpp *.h README.TXT utils/*.h utils/*.cpp 3rdparty/ --exclude='*git*'
+tar -czf ihcserver-$1.tar.gz *.cpp *.h README.TXT webinterface/* utils/*.h utils/*.cpp 3rdparty/ --exclude='*git*'
